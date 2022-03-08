7651e180e5909b4b4fc57082e2e67f5ab5e039f8
Moi <azuly@laptop>
2022-03-08 13:22:47 +0100

    modificaciones
    Signed-off-by: Moi <azuly@laptop>

--- pruebagit/src/pruebagit/pruebecilla.java
+++ pruebagit/src/pruebagit/pruebecilla.java
@@ -5,6 +5,10 @@ public class pruebecilla {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+		System.out.println("Hola");
+
+		System.out.println("Hola");
+
 
 	}
 
